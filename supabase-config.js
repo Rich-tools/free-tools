f2b1aa2e9d49bf29f04c709fee06e8ff519237e0
kidume105 <richardngaye105@gmail.com>
2026-03-07 04:08:24 +0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://gmpkykhafcqvnkjspnhs.supabase.co";
+const SUPABASE_KEY = "sb_publishable_CJZE32YWFEf1nyFl3zhYeQ_andbB3Ad";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
